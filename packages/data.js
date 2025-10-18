2bc11f9c5f2571e2451c8fd3fb0911ed362bd24d
ElijahChaney966 <stefaniechaney992@gmail.com>
2025-10-18 16:19:07 -0500

    Create data.js with user and username mappings
    Fix issues in user and username mapping logic.

---
+++ packages/data.js
@@ -0,0 +1,24 @@
+export default () => {
+        var name = users.map(user => users.name)
+
+    var users = [
+        {name: "Elijah",
+        name: "Tim",
+        name: "Stefanie"}
+    ];
+
+    var usernames = [
+        {usernames: "elijah2017",
+            usernames: "stefanie1982",
+            usernames: "tim1958"
+        }
+    ];
+
+    for (const user of usernames) {
+        usernames.push(user.name)
+    }
+    for (var user_name of users){
+        users.push(users.name)
+    }
+}
+
